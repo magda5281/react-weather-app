1e07833db4a0475f95efd3cbcf46b89bbba19cbc
Your Name <you@example.com>
2021-05-13 11:18:10 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
 
+
 export default function App() {
   return (
     <div className="App">
-     <h1>Weather App </h1>
+      <div className="container"> 
+        <h1>Weather App </h1>
+        <footer>
+        This project is coded by <a href = "https://www.linkedin.com/in/magdalena-gozdalik-3458202b/" rel="noreferrer" >Magda Gozdalik </a> and is <a href = "https://github.com/magda5281/react-weather-app" target="_blank"rel="noreferrer"> open-sourced on GitHub</a>.
+        </footer>
+      </div>
     </div>
   );
 }
